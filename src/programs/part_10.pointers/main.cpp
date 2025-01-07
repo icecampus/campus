6b4baac0eb1f8b9e52120dac85c9f6ee7e388111
ice_rezzet <ice.rezzet@gmail.com>
2025-01-07 10:41:50 +0300

    pointers example

--- src/programs/part_10.pointers/main.cpp
+++ src/programs/part_10.pointers/main.cpp
@@ -2,5 +2,20 @@
 
 int main()
 {
+	int val = 10;
+	float fVal = 10.f;
+	void* ptr = 0;
+
+
+	ptr = &val;
+	ptr = &fVal;
+
+
+	//std::cout << "value: " << *ptr << "\n";
+
+	//*ptr = 20;
+
+	//std::cout << "value: " << *ptr << "\n";
+	std::cout << "val: " << val << "\n";
 
 }
